5c632ac4121fee2733f96430729491466884d9db
aparnah18 <aparnah18@gmail.com>
2021-01-06 19:36:49 +0530

    Added username and password inside Login form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'fontsource-roboto';
 import registerServiceWorker from './registerServiceWorker';
-import Home from './screens/home/Home'
+import Home from './screens/home/Home';
 
 ReactDOM.render(
   <Home />,
